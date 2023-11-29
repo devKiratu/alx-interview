98fca8fc57be18d3be12500e49596d7c59e529fb
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-11-29 16:27:55 +0300

    add brute force solution to Pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,41 @@
+"""
+Algorithm for building a Pascal's triangle
+"""
+
+
+def getRow(prev):
+    """
+    Helper function for building a row in Pascal's triangle from the
+    preceding row
+    """
+    row = []
+    if len(prev) == 1:
+        return [1, 1]
+    row.append(1)
+
+    i = 0
+    while i < len(prev) - 1:
+        nextNum = prev[i] + prev[i + 1]
+        row.append(nextNum)
+        i += 1
+    row.append(1)
+    return row
+
+
+def pascal_triangle(n):
+    """returns a list representation of Pascal's triangle of depth n"""
+    triangle = []
+    prev = []
+    if n <= 0:
+        triangle.append([])
+    if n >= 1:
+        prev.append(1)
+        triangle.append(prev)
+
+    i = 2
+    while i <= n:
+        current = getRow(prev)
+        triangle.append(current)
+        prev = current
+        i += 1
+    return triangle
