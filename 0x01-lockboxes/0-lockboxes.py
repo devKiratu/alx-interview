9ee84b3d4c32ba162baf2eb47ac5a652ed680c4b
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-07 17:35:40 +0300

    set up project files

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+You have n number of locked boxes in front of you. Each box is numbered
+sequentially from 0 to n - 1 and each box may contain keys to the other boxes
+This module contains the solution to the above puzzle
+"""
+
+
+def canUnlockAll(boxes):
+    """determines if all the boxes can be opened"""
+    pass
