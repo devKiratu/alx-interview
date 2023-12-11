53c72a736013362d54c17e11352ccdf88979a31d
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-11 14:52:08 +0300

    write helper functions

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,4 +10,28 @@ def minOperations(n: int) -> int:
     Returns the fewest steps required to print H exactly n times
     or 0 if impossible
     """
-    return 0
+    total_printed: int = 1
+    clipboard: str = ''
+    steps = 0
+
+    def copy_all():
+        """helper function to simulate copy operations"""
+        nonlocal clipboard, steps
+        if len(clipboard) == 0:
+            clipboard += "H"
+        else:
+            clipboard += clipboard
+        steps += 1
+    
+    def paste():
+        """helper function to simulate paste operation"""
+        nonlocal total_printed, steps
+        total_printed += len(clipboard)
+        steps += 1
+
+    if n <= 1:
+        return 0
+    
+    # TODO: write algorithm to decide whether to copy+paste or paste
+    
+    return steps
