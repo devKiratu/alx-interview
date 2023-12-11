b09ea2bc1dbb3ab18b91111bca10df8bd5835111
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-11 13:39:31 +0300

    add starter files

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains a function that determines the minimum number of
+operations required to perform a task
+"""
+
+
+def minOperations(n: int) -> int:
+    """
+    Returns the fewest steps required to print H exactly n times
+    or 0 if impossible
+    """
+    return 0
