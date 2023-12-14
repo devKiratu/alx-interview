2a1e4129b28044fc9f527ffdedb012d45435b713
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-14 23:55:23 +0300

    refactor: optimize algorithm and remove redundant code

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,37 +7,19 @@ operations required to perform a task
 
 def minOperations(n: int) -> int:
     """
-    Returns the fewest steps required to print H exactly n times
-    or 0 if impossible
+    Returns the fewest steps required to print H exactly n times or 0
     """
     total_printed: int = 1
     clipboard: int = 0
     steps = 0
 
-    def copy_all():
-        """helper function to simulate copy operations"""
-        nonlocal clipboard, steps
-        if clipboard == 0:
-            clipboard += 1
-        else:
-            clipboard += clipboard
-        steps += 1
-
-    def paste():
-        """helper function to simulate paste operation"""
-        nonlocal total_printed, steps
+    while total_printed < n:
+        if n % total_printed == 0:
+            # copy_all
+            clipboard = total_printed
+            steps += 1
+        # paste
         total_printed += clipboard
         steps += 1
 
-    if n <= 1:
-        return 0
-
-    while total_printed < n:
-        if clipboard == 0:
-            copy_all()
-            paste()
-        if (n - total_printed) % total_printed == 0:
-            copy_all()
-        paste()
-
-    return steps
+    return steps if total_printed == n else 0
