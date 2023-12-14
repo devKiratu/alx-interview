e794fb4d6a31826017e08bc0f85f5888bcb2bcb2
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-15 00:07:27 +0300

    remove typing info

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,12 +5,12 @@ operations required to perform a task
 """
 
 
-def minOperations(n: int) -> int:
+def minOperations(n):
     """
     Returns the fewest steps required to print H exactly n times or 0
     """
-    total_printed: int = 1
-    clipboard: int = 0
+    total_printed = 1
+    clipboard = 0
     steps = 0
 
     while total_printed < n:
