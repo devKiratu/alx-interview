77aa9e91b3275030fbbe529c25b62937d87b01d1
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-19 14:14:13 +0300

    add interim solution

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,4 +3,52 @@
 This script reads stdin line by line and computes metrics organized as follows:
     file size
     http status code, total number
+Possible status codes: 200, 301, 400, 401, 403, 404, 405 and 500
 """
+import sys
+import traceback
+
+
+counter = 0
+file_size = 0
+status_map = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+
+
+def print_stats():
+    """Print the currents statistics"""
+    print(f"File size: {file_size}")
+    for status, count in status_map.items():
+        if count > 0:
+            print(f"{status}: {count}")
+            sys.stdout.flush()
+    sys.stdout.flush()
+
+
+try:
+    for line in sys.stdin:
+        if counter == 10:
+            print_stats()
+            counter = 0
+        line = line.rstrip()
+        file_size += len(line)
+        code_str = str.rfind(line, '1.1')
+        start = code_str + 5
+        end = start + 3
+        code = line[start:end]
+        if status_map.get(code, None) is not None:
+            current = status_map[code]
+            status_map[code] = current + 1
+        counter += 1
+except KeyboardInterrupt as e:
+    print_stats()
+    sys.stdout.flush()
+    traceback.print_exc(file=sys.stdout)
