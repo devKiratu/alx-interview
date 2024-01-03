1b05031e09699a692bdb6a5129a074a9f8f6e95c
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-03 15:07:27 +0300

    update validation logic to use utf8 starting bytes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,6 +11,9 @@ def validUTF8(data):
     last_8 = []
     for item in data:
         bin_item = bin(item)[2:]
-        int_item = int(bin_item[-8:], 2)
-        last_8.append(int_item)
-    return all(i > 0 and i <= 255 for i in last_8)
+        last_8.append(bin_item[-8:])
+    return all(i[0] == '0' or
+               i[0:2] == '10' or
+               i[0:3] == '110' or
+               i[0:4] == '1110' or
+               i[0:5] == '11110' for i in last_8)
