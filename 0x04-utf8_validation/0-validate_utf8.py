144aeea4f373d48434ca32124c8b9a965a8fa832
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-03 17:47:08 +0300

    use variable length of utf8 characters in validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,12 +8,39 @@ def validUTF8(data):
     """
     determines if a given data set represents a valid UTF-8 encoding
     """
-    last_8 = []
+    bin_data = []
     for item in data:
         bin_item = bin(item)[2:]
-        last_8.append(bin_item[-8:])
-    return all(i[0] == '0' or
-               i[0:2] == '10' or
-               i[0:3] == '110' or
-               i[0:4] == '1110' or
-               i[0:5] == '11110' for i in last_8)
+        bin_data.append(bin_item[-8:])
+    result = []
+    i = 0
+    try:
+        while i < len(bin_data):
+            # check for 1-byte character
+            if len(bin_data[i]) <= 7:
+                i += 1
+                continue
+            # check for 2-byte character
+            if bin_data[i][0:3] == '110':
+                if bin_data[i + 1][0:2] == '10':
+                    i += 2
+            else:
+                return False
+            # check for 3-byte character
+            if bin_data[i][0:4] == '1110':
+                if bin_data[i + 1][0:2] == '10' and\
+                 bin_data[i + 2][0:2] == '10':
+                    i += 3
+                else:
+                    return False
+            # check for 4-byte character
+            if bin_data[i][0:5] == '11110':
+                if bin_data[i + 1][0:2] == '10' and\
+                  bin_data[i + 2][0:2] == '10' and\
+                  bin_data[i + 3][0:2] == '10':
+                    i += 4
+                else:
+                    return False
+        return True
+    except Exception:
+        return False
