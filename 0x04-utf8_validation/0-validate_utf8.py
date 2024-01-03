06de4c64f5594260745bcf71825cba95e270b21d
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-03 14:10:10 +0300

    use the last 8 bytes for utf8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,4 +8,9 @@ def validUTF8(data):
     """
     determines if a given data set represents a valid UTF-8 encoding
     """
-    return all(i >= 0 and i <= 255 for i in data)
+    last_8 = []
+    for item in data:
+        bin_item = bin(item)[2:]
+        int_item = int(bin_item[-8:], 2)
+        last_8.append(int_item)
+    return all(i > 0 and i <= 255 for i in last_8)
