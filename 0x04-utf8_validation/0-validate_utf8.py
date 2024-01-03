8d6815014928235f2e528e3ee1f1c80bf2ae8e89
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-03 13:39:30 +0300

    add starter files

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines a method for validating if data is of valid utf8 encoding
+"""
+
+
+def validUTF8(data):
+    """
+    determines if a given data set represents a valid UTF-8 encoding
+    """
+    return True
