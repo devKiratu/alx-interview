dadb551784e3edfdadb40d58d9bafb6f5a5e3c84
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-25 10:42:20 +0300

    add solution to rotate 2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,4 +12,19 @@ def rotate_2d_matrix(matrix):
     """
     rotates a 2d matrix in place
     """
-    pass
+    n = len(matrix)
+
+    # Transpose the matrix - sets each row in final position but in
+    # reversed order
+    for r in range(n):
+        for c in range(r, n):
+            matrix[c][r], matrix[r][c] = matrix[r][c], matrix[c][r]
+
+    # reverse each row in the matrix
+    for row in matrix:
+        left = 0
+        right = len(row) - 1
+        while left < right:
+            row[left], row[right] = row[right], row[left]
+            left += 1
+            right -= 1
