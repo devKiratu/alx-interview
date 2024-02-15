c8e948f2875f4dcb5444fefc325ab8646d750f55
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-02-15 11:01:05 +0300

    define helper functions inside main func

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -55,6 +55,21 @@ def isWinner(x, nums):
     maria = 0
     i = 0
 
+    def generate_primes(n):
+        """
+        returns a list of prime numbers from 1 upto and including n
+        """
+        primes = []
+        for i in range(2, n+1):
+            is_prime = True
+            for j in range(2, int(i**0.5) + 1):
+                if i % j == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                primes.append(i)
+        return primes
+
     while i < x:
         primes = generate_primes(nums[i])
         if len(primes) % 2 == 0:
@@ -69,19 +84,3 @@ def isWinner(x, nums):
         return 'Maria'
     else:
         return None
-
-
-def generate_primes(n):
-    """
-    returns a list of prime numbers from 1 upto and including n
-    """
-    primes = []
-    for i in range(2, n+1):
-        is_prime = True
-        for j in range(2, int(i**0.5) + 1):
-            if i % j == 0:
-                is_prime = False
-                break
-        if is_prime:
-            primes.append(i)
-    return primes
